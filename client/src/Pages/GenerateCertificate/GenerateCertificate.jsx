1ce054b1065f23c59228aa20e218b765949aed12
ShayanJamil519 <shayanjamil500@gmail.com>
2023-04-30 12:06:01 +0500

    Download as PNG Functionality Done

--- client/src/Pages/GenerateCertificate/GenerateCertificate.jsx
+++ client/src/Pages/GenerateCertificate/GenerateCertificate.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./GenerateCertificate.css";
+import html2canvas from "html2canvas";
+// import html2pdf from "html2pdf.js";
 import Navbar from "../../Components/Navbar/Navbar";
 
 const GenerateCertificate = () => {
+  const [certificateHolderName, setCertificateHolderName] = useState("");
+  const [certificateName, setCertificateName] = useState("");
+  const certificate = document.getElementById("certificate");
+
+  const handleCertificateDownloadPNG = () => {
+    html2canvas(certificate).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const link = document.createElement("a");
+      link.download = "certificate.png";
+      link.href = imgData;
+      link.click();
+    });
+  };
+
+  // const handleCertificateDownloadPDF = () => {
+  //   const options = {
+  //     margin: 0,
+  //     filename: "certificate.pdf",
+  //     image: { type: "png", quality: 1 },
+  //     html2canvas: { scale: 2 },
+  //     jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+  //   };
+
+  //   html2pdf().set(options).from(certificate).save();
+  // };
+
   return (
     <div>
       {/* Navbar */}
@@ -19,6 +47,8 @@ const GenerateCertificate = () => {
               id="name"
               required
               placeholder="e.g: John Daud"
+              value={certificateHolderName}
+              onChange={(e) => setCertificateHolderName(e.target.value)}
             />
           </div>
           <div>
@@ -37,6 +67,8 @@ const GenerateCertificate = () => {
               id="credentials"
               required
               placeholder="e.g: Certified Ethical Hacker - Academia (PCP)"
+              value={certificateName}
+              onChange={(e) => setCertificateName(e.target.value)}
             />
           </div>
           <div>
@@ -45,20 +77,25 @@ const GenerateCertificate = () => {
           </div>
         </form>
         <div className="btns__container">
+          {/* <button onClick={handleCertificateDownloadPDF}>
+            Generate and download as PDF
+          </button> */}
           <button>Generate and download as PDF</button>
-          <button>Generate and download as PNG</button>
+          <button onClick={handleCertificateDownloadPNG}>
+            Generate and download as PNG
+          </button>
         </div>
       </div>
 
       {/* Certificate Preview Container */}
       <div className="certificate__preview__container">
         <h1>Certificate Preview</h1>
-        <div className="certificate__preview">
+        <div className="certificate__preview" id="certificate">
           <div className="name__container">
-            <h1>John Daud</h1>
+            <h1>{certificateHolderName}</h1>
           </div>
           <div className="credentials__container">
-            <h2>Certified Ethical Hacker - Academia (PCP)</h2>
+            <h2>{certificateName}</h2>
             <h3>
               built in partnership with & endorsed by University Kuala Lumpur
             </h3>
@@ -70,3 +107,5 @@ const GenerateCertificate = () => {
 };
 
 export default GenerateCertificate;
+
+// Certified Ethical Hacker - Academia (PCP)
