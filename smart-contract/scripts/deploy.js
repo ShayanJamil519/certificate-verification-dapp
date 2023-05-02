1bf91ef666449df9da563c009ced18dd843bf42d
ShayanJamil519 <shayanjamil500@gmail.com>
2023-05-02 21:39:01 +0500

    Hardhat setup done

---
+++ smart-contract/scripts/deploy.js
@@ -0,0 +1,12 @@
+const hre = require("hardhat");
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
+async function main() {}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
