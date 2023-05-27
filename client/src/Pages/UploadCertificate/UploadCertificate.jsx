798fde60fd6d73f35eb5f0378d8494d385cc3d1e
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-05-27 20:53:19 +0500

    production issue solved

--- client/src/Pages/UploadCertificate/UploadCertificate.jsx
+++ client/src/Pages/UploadCertificate/UploadCertificate.jsx
@@ -51,7 +51,8 @@ const UploadCertificate = () => {
       // Adding our file to ipfs
       const added = await ipfs.add(uploadFile);
       let certificateHash = added.path;
-      setCertificateHash(certificateHash);
+      console.log("certificateHash: ", certificateHash);
+      // setCertificateHash(certificateHash);
 
       // Making connection to the blockchain, getting signer wallet address and connecting to our smart contract
       const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -128,59 +129,6 @@ const UploadCertificate = () => {
   };
 
   // ======================================
-
-  const handleEmailSend = async (e) => {
-    // e.preventDefault();
-    console.log("handle email called");
-    // Getting Student Name and Student Email from localstorage
-    let studentName, studentEmail;
-    if (
-      localStorage.getItem("studentName") &&
-      localStorage.getItem("studentEmail")
-    ) {
-      studentName = localStorage.getItem("studentName");
-      studentEmail = localStorage.getItem("studentEmail");
-    } else {
-      return;
-    }
-
-    // // Production Environement
-    const templateParams = {
-      to_name: studentName,
-      to_email: studentEmail,
-      message: `We are thrilled to extend our warmest congratulations to you for successfully completing your course.  To view and download your certificate, simply click on the following link: [ https://certificate-generator-verifier.netlify.app/download_certificate/${certificateHash} ]. This secure and decentralized link ensures the integrity and immutability of your certificate, providing you with a reliable record of your accomplishment.  Additionally, to validate the authenticity of your microcredential, you can visit our website's verification page by clicking on this direct link: [ https://certificate-generator-verifier.netlify.app/verify_certificate ] and past your HashID [ ${certificateHash} ]. This page will allow you to confirm your certificate's details and ensure its validity for potential employers, colleagues, or other interested parties.       We commend your commitment to continuous learning and professional development. Your newly acquired skills and knowledge from the course will undoubtedly enhance your career prospects and contribute to your success.
-      `,
-    };
-
-    // QmX6muprkU3oRQCQtarHrZhP7Y8xJGYKKNQ9pWUUwLXQbc
-    // http://localhost:3000/download_certificate/QmX6muprkU3oRQCQtarHrZhP7Y8xJGYKKNQ9pWUUwLXQbc
-
-    // Test Environement
-    // const templateParams = {
-    //   to_name: studentName,
-    //   to_email: studentEmail,
-    //   message: `We are thrilled to extend our warmest congratulations to you for successfully completing your course.  To view and download your certificate, simply click on the following link: [ http://localhost:3000/download_certificate/${certificateHash} ]. This secure and decentralized link ensures the integrity and immutability of your certificate, providing you with a reliable record of your accomplishment.  Additionally, to validate the authenticity of your microcredential, you can visit our website's verification page by clicking on this direct link: [ https://certificate-generator-verifier.netlify.app/verify_certificate ] and past your HashID [ ${certificateHash} ]. This page will allow you to confirm your certificate's details and ensure its validity for potential employers, colleagues, or other interested parties.       We commend your commitment to continuous learning and professional development. Your newly acquired skills and knowledge from the course will undoubtedly enhance your career prospects and contribute to your success.
-    //   `,
-    // };
-
-    // emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-    emailjs
-
-      .send(
-        "service_x7393hg",
-        "template_91gjr3s",
-        templateParams,
-        "zF2fKmlCZB5Z_fJKq"
-      )
-      .then(
-        () => {
-          toast.success("Email sent to student");
-        },
-        (error) => {
-          toast.error(error.text);
-        }
-      );
-  };
 
   // ==========================
 
